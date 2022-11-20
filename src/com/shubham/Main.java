629b2c1e1dfc53461c52a760b3748bcf2402b02d
Shubham Raut <rautshubham3242@gmail.com>
2022-11-20 22:29:23 +0530

    modified Main.java

--- src/com/shubham/Main.java
+++ src/com/shubham/Main.java
@@ -1,7 +1,23 @@
 package com.shubham;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        static final String DB_URL = "jdbc:mysql://localhost/";
+        static final String USER = "admin";
+        static final String PASS = "admin123";
+        public static void main(String[] args){
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ){
+                String sql = "CREATE DATABASE IPL";
+                stmt.executeUpdate(sql);
+                System.out.println("Database created successfully...");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
