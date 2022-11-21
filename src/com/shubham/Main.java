6102b391590c7da86c5a10bc6c8d02cb831768cd
Shubham Raut <rautshubham3242@gmail.com>
2022-11-21 22:30:14 +0530

    modified Main.java

--- src/com/shubham/Main.java
+++ src/com/shubham/Main.java
@@ -1,108 +1,38 @@
 package com.shubham;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
-        static final String DB_URL = "jdbc:mysql://localhost/";
-        static final String USER = "admin";
-        static final String PASS = "admin123";
+    static final String DB_URL = "jdbc:mysql://localhost/";
+    static final String USER = "admin";
+    static final String PASS = "admin123";
 
-        static String createDatabaseQuery = "CREATE DATABASE ipl3";
-        static String useDatabaseQuery = "USE ipl3";
-        static String createTableQuery = "CREATE TABLE demo(id INTEGER, name varchar(10))";
-        //static String insertValueQuery1 = "insert into demo values(11, 'Shubham')";
-        //static String insertValueQuery2 = "insert into demo values(12, 'Sumit')";
-        static String matchesFileScriptPath = "src/com/shubham/matches.sql";
-        static String deliveriesFileScriptPath = "src/com/shubham/matches.sql";
-
-
+    static final String numberOfMatchesPlayedPerYearQuery = "SELECT season, count(season) FROM matches GROUP BY season;";
+    static final String NumberOfMatchesWonOfAllTeamQuery = "SELECT winner, count(winner) FROM matches GROUP BY winner;";
+    static final String extraRunsConcededPerTeamIn2016Query = "SELECT bowling_team, sum(extra_runs) FROM deliveries WHERE match_id in ( SELECT id FROM matches WHERE season = 2016) GROUP BY bowling_team;";
+    static final String mostEconomicalBowlerIn2015Query = "SELECT bowler, sum(wide_runs+ noball_runs+batsman_runs)*6/count(all(wide_runs+noball_runs=0))  FROM deliveries WHERE match_id in (SELECT id FROM matches WHERE season=2015) GROUP BY bowler;";
     public static void main(String[] args){
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement statement = conn.createStatement();
             ){
+            try {
+                HashMap<String, Integer> matchesPlayedAllYear = findNumberOfMatchesPlayedPerYear(statement);
+                HashMap<String, Integer> matchesWonAllTeam = findNumberOfMatchesWonOfAllTeam(statement);
+                HashMap<String, Integer> extraRunsConcededPerTeamIn2016 = findExtraRunsConcededPerTeam(statement, deliveries);
+                HashMap<String, Integer> mostEconomicalBowlerIn2015 = findTheMostEconomicalBowlerIn2015(statement, deliveries);
+                HashMap<String, Integer> teamWhoWonTheTossAndMatch = findTheTeamWhoWonTheTossAndWonTheMatch(statement);
 
-                createDatabase(statement, createDatabaseQuery);
-                useDatabase(statement, useDatabaseQuery);
-                createTable(statement, createTableQuery);
-                createMatchesTable(statement, matchesFileScriptPath);
-                createDeliveriesTable(statement, deliveriesFileScriptPath);
-
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
-
-
-    private static void createMatchesTable(Statement statement, String matchesFileScriptPath) {
-        try {
-            statement.executeUpdate("SOURCE "+matchesFileScriptPath);
-            System.out.println("matches table created Successfully");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private static void createDeliveriesTable(Statement statement, String deliveriesFileScriptPath) {
-        try {
-            statement.executeUpdate("SOURCE "+deliveriesFileScriptPath);
-            System.out.println("Table created Successfully");
-            System.out.println("deliveries table created Successfully");
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void createDatabase(Statement statement, String createDatabaseQuery){
-        try {
-            statement.executeUpdate(Main.createDatabaseQuery);
-            System.out.println("Database created Successfully");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void useDatabase(Statement statement, String useDatabaseQuery){
-        try {
-            statement.executeUpdate(useDatabaseQuery);
-            System.out.println("Database Changed");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void createTable(Statement statement, String createTableQuery){
-        try {
-            statement.executeUpdate(createTableQuery);
-            System.out.println("Table Created Successfully");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static void insertValue(Statement statement, String insertValueQuery){
-        try {
-            statement.executeUpdate(insertValueQuery);
-            System.out.println("Value Inserted Successfully");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-
-//    public static void convertCsvtoSqlFile(Statement statement, String path){
-//            String sql = "";
-//            statement.executeUpdate();
-//    }
-
-
 }
