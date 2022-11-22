13a8d5c14315f047a468d7fdce80e195065b9d4d
Shubham Raut <rautshubham3242@gmail.com>
2022-11-22 13:57:07 +0530

    modified Main.java

--- src/com/shubham/Main.java
+++ src/com/shubham/Main.java
@@ -10,41 +10,6 @@ public class Main {
     static final String USER = "admin";
     static final String PASS = "admin123";
 
-//    public static final int MATCH_ID = 0;
-//    public static final int MATCH_SEASON = 1;
-//    public static final int MATCH_CITY = 2;
-//    public static final int MATCH_DATE = 3;
-//    public static final int MATCH_TEAM1 = 4;
-//    public static final int MATCH_TEAM2 = 5;
-//    public static final int MATCH_TOSS_WINNER = 6;
-//    public static final int MATCH_TOSS_DECISION = 7;
-//    public static final int MATCH_RESULT = 8;
-//    public static final int MATCH_DL_APPLIED = 9;
-//    public static final int MATCH_WINNER = 10;
-//    public static final int MATCH_WIN_BY_RUNS = 11;
-//    public static final int MATCH_WIN_BY_WICKETS = 12;
-//    public static final int MATCH_PLAYER_OF_MATCH = 13;
-//    public static final int MATCH_VENUE = 14;
-//
-//    public static final int DELIVERY_MATCH_ID = 0;
-//    public static final int DELIVERY_INNING = 1;
-//    public static final int DELIVERY_BATTING_TEAM = 2;
-//    public static final int DELIVERY_BOWLING_TEAM = 3;
-//    public static final int DELIVERY_OVER = 4;
-//    public static final int DELIVERY_BALL = 5;
-//    public static final int DELIVERY_BATSMAN = 6;
-//    public static final int DELIVERY_NON_STRIKER = 7;
-//    public static final int DELIVERY_BOWLER = 8;
-//    public static final int DELIVERY_IS_SUPER_OVER = 9;
-//    public static final int DELIVERY_WIDE_RUNS = 10;
-//    public static final int DELIVERY_BYE_RUNS = 11;
-//    public static final int DELIVERY_LEG_BYE_RUNS = 12;
-//    public static final int DELIVERY_NO_BALL_RUNS = 13;
-//    public static final int DELIVERY_PENALTY_RUNS = 14;
-//    public static final int DELIVERY_BATSMAN_RUNS = 15;
-//    public static final int DELIVERY_EXTRA_RUNS = 16;
-//    public static final int DELIVERY_TOTAL_RUNS = 17;
-
     public static void main(String[] args) {
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
              Statement statement = conn.createStatement();
